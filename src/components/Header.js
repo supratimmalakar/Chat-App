ce805e71a21af89ec7ea55e9d59a223b628a6eb5
Supratim Malakar <supratimmalakar5@gmail.com>
2022-01-20 00:37:35 +0530

    Added Logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     AppBar,
     Toolbar,
     Typography,
     Grid,
-    makeStyles
+    makeStyles,
+    IconButton,
+    Menu,
+    MenuItem,
 } from '@material-ui/core'
 import AddContact from './AddContact'
 import Requests from './Requests'
+import { auth } from '../firebase'
+import { LogoutIcon } from '../assets/CustomIcons'
 
 const useStyles = makeStyles({
-    navbar : {
-        padding : '10px',
-        height : '9vh'
+    navbar: {
+        padding: '10px',
+        height: '9vh'
     },
-    displayImage : {
-        width : '60px',
-        height : '60px',
-        borderRadius : '50%'
+    displayImage: {
+        width: '60px',
+        height: '60px',
+        borderRadius: '50%'
     },
-    headerOptions : {
-        display : 'flex',
-        flexDirection : 'row'
+    headerOptions: {
+        display: 'flex',
+        flexDirection: 'row'
     }
 })
 
 function Header({ user }) {
+    const [anchorEl, setAnchorEl] = useState(null)
     const classes = useStyles()
     return (
-        <AppBar className={classes.navbar} position='static'>
-            <Toolbar>
-                <Grid container direction='row' justifyContent='space-between' alignItems='center'>
-                    <Typography variant="h3">Chat App</Typography>
-                    <Grid container item xs={2} direction='row' justifyContent='space-between'>
-                        <AddContact user={user}/>
-                        <Requests user={user}/>
-                        <img className={classes.displayImage} src={user.photoURL}/>
+        <>
+            <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={() => setAnchorEl(null)}
+            >
+                <MenuItem onClick={() => auth.signOut()}>
+                    <LogoutIcon color="rgba(0,0,0,0.5)"/>
+                      Logout
+                </MenuItem>
+            </Menu>
+            <AppBar className={classes.navbar} position='static'>
+                <Toolbar>
+                    <Grid container direction='row' justifyContent='space-between' alignItems='center'>
+                        <Typography variant="h3">Chat App</Typography>
+                        <Grid container item xs={2} direction='row' justifyContent='space-between'>
+                            <AddContact user={user} />
+                            <Requests user={user} />
+                            <IconButton onClick={event => setAnchorEl(event.currentTarget)}>
+                                <img className={classes.displayImage} src={user.photoURL} />
+                            </IconButton>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Toolbar>
-        </AppBar>
+                </Toolbar>
+            </AppBar>
+        </>
+
     )
 }
 
